1fed8350c68ead3defd91ca85971f40bcee04114
Stéphane Chorne <stephane.chorne@gmail.com>
2018-05-22 01:11:22 +0200

    Fix failing integration test

--- test/integration.js
+++ test/integration.js
@@ -8,15 +8,16 @@ describe('Cloud Firestore', () => {
     test('Initialize app', () => {
         firebase.initializeApp({
             apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_API_KEY,
-            projectId: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.FIREBASE_PROJECT_ID,
         });
     });
 
-    test('Insert and read data', () => {
+    test('Add and delete data', () => {
         const db = firebase.firestore();
         db.settings({timestampsInSnapshots: true});
 
-        return db.collection('test').add({foo: 'bar'});
+        return db.collection('test').add({foo: 'bar'})
+            .then(ref => ref.delete());
     });
 });
