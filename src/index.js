3aaa1e28417007948530f6229d3959a202aab32d
Stéphane Chorne <stephane.chorne@gmail.com>
2018-05-23 04:16:03 +0200

    Initial implementation with unit tests

---
+++ src/index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// Max batch operations. See https://firebase.google.com/docs/firestore/quotas.
+const BATCH_LIMIT = 500;
+
+/**
+ * Provides the regular batch operation methods of Firestore but uses multiple
+ * batch instances internally to work around the batch limit of 500 operations.
+ */
+class MultiBatch {
+    /**
+     * @param {firebase.firestore} db
+     * @param {number} limit
+     */
+    constructor(db, limit = BATCH_LIMIT) {
+        this.db = db;
+        this.limit = limit < BATCH_LIMIT ? limit : BATCH_LIMIT;
+        this.batches = [db.batch()];
+        this.operations = 0;
+    }
+
+    /**
+     * @returns {firebase.firestore.WriteBatch}
+     */
+    getBatch() {
+        if (this.operations < this.limit) {
+            return this.batches[this.batches.length - 1];
+        }
+
+        const batch = this.db.batch();
+        this.batches.push(batch);
+        this.operations = 0;
+
+        return batch;
+    }
+
+    /**
+     * @returns {Promise}
+     */
+    commit() {
+        return Promise.all(this.batches.map(batch => batch.commit()))
+            .then(() => {
+                this.batches = [this.db.batch()];
+                this.operations = 0;
+            });
+    }
+
+    /**
+     * @param {firebase.firestore.DocumentReference} ref
+     * @returns {MultiBatch}
+     */
+    delete(ref) {
+        this.getBatch().delete(ref);
+        this.operations++;
+
+        return this;
+    }
+
+    /**
+     * @param {firebase.firestore.DocumentReference} ref
+     * @param {Object} data
+     * @param {firebase.firestore.SetOptions} options
+     * @returns {MultiBatch}
+     */
+    set(ref, data, options) {
+        this.getBatch().set(ref, data, options);
+        this.operations++;
+
+        return this;
+    }
+
+    /**
+     * @param {firebase.firestore.DocumentReference} ref
+     * @param {*} args
+     * @returns {MultiBatch}
+     */
+    update(ref, ...args) {
+        this.getBatch().update(ref, ...args);
+        this.operations++;
+
+        return this;
+    }
+}
+
+module.exports = {
+    MultiBatch,
+    BATCH_LIMIT,
+};
--- test/index.js
+++ test/index.js
@@ -1,3 +1,85 @@
-/**
- * Use this file to perform test setups after the test framework has loaded.
- */
+'use strict';
+
+const {MultiBatch, BATCH_LIMIT} = require('../src/index');
+
+const db = {
+    batch() {
+        return {
+            commit() {},
+            delete() {},
+            set() {},
+            update() {},
+        };
+    },
+};
+
+describe('MultiBatch', () => {
+    describe('constructor', () => {
+        test('with default limit', () => {
+            const mb = new MultiBatch(db);
+            const batch = mb.getBatch();
+
+            expect(mb.limit).toBe(BATCH_LIMIT);
+            expect(mb.operations).toBe(0);
+
+            expect(batch).toBeDefined();
+            expect(mb.batches.length).toBe(1);
+            expect(batch).toBe(mb.batches[0]);
+        });
+
+        test('with custom limit under BATCH_LIMIT', () => {
+            const limit = BATCH_LIMIT - 1;
+            const mb = new MultiBatch(db, limit);
+            expect(mb.limit).toBe(limit);
+        });
+
+        test('with custom limit over BATCH_LIMIT', () => {
+            const limit = BATCH_LIMIT + 1;
+            const mb = new MultiBatch(db, limit);
+            expect(mb.limit).toBe(BATCH_LIMIT);
+        });
+    });
+
+    test('getBatch', () => {
+        const mb = new MultiBatch(db, 1);
+
+        const batch1 = mb.getBatch();
+        mb.set();
+        expect(batch1).toBe(mb.batches[0]);
+
+        const batch2 = mb.getBatch();
+        mb.set();
+        expect(batch2).toBe(mb.batches[1]);
+    });
+
+    test('commit', () => {
+        const mb = new MultiBatch(db, 1);
+        mb.set();
+        mb.set();
+
+        expect(mb.batches.length).toBe(2);
+        expect(mb.operations).toBe(1);
+
+        return mb.commit()
+            .then(() => {
+               expect(mb.batches.length).toBe(1);
+               expect(mb.operations).toBe(0);
+            });
+    });
+
+    test('delete, set, update', () => {
+        const mb = new MultiBatch(db, 2);
+        mb.set();
+        mb.delete();
+        mb.update();
+
+        expect(mb.batches.length).toBe(2);
+        expect(mb.operations).toBe(1);
+
+        return mb.commit()
+            .then(() => {
+                expect(mb.batches.length).toBe(1);
+                expect(mb.operations).toBe(0);
+            });
+    });
+});
